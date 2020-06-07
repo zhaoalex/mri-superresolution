resize imgs to whole num res before starting algos + fix raisr for different scaling factors

raisr/raisr_fulltest.py
@@ -95,6 +95,12 @@ imagecount = 1
 for img_path in imagelist:
     hr = io.imread(img_path)
     hr_gray = rgb2gray(hr)
+
+    # make sure downscale will divide evenly
+    old_scale = hr_gray.shape
+    new_scale = ((old_scale[0] // scaling_factor) * scaling_factor, (old_scale[1] // scaling_factor) * scaling_factor)
+    hr_gray = transform.resize(hr_gray, new_scale)
+
     image = downscale(hr_gray, scaling_factor)
     image = img_as_ubyte(image)
 
@@ -116,8 +122,8 @@ for img_path in imagelist:
     heightgridLR = np.linspace(0,heightLR-1,heightLR)
     widthgridLR = np.linspace(0,widthLR-1,widthLR)
     bilinearinterp = interpolate.interp2d(widthgridLR, heightgridLR, grayorigin, kind='linear')
-    heightgridHR = np.linspace(0,heightLR-0.5,heightLR*2)
-    widthgridHR = np.linspace(0,widthLR-0.5,widthLR*2)
+    heightgridHR = np.linspace(0,heightLR-0.5,heightLR*R)
+    widthgridHR = np.linspace(0,widthLR-0.5,widthLR*R)
     upscaledLR = bilinearinterp(widthgridHR, heightgridHR)
     # Calculate predictHR pixels
     heightHR, widthHR = upscaledLR.shape