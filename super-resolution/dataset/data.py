4a2c134b9d39c973393174144bf18fc4e4141f24
Alex Zhao <alexzhao21@gmail.com>
2020-06-07 01:15:58 -0700

    data augmentation + remove bsds300

--- super-resolution/dataset/data.py
+++ super-resolution/dataset/data.py
@@ -3,31 +3,40 @@ from os import remove
 from os.path import exists, join, basename
 
 from six.moves import urllib
-from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize
+from torchvision.transforms import Compose, CenterCrop, ToTensor, Resize, RandomHorizontalFlip
+import torchvision.transforms.functional as TF
+import random
 
 from .dataset import DatasetFromFolder
 
+class RandomDiscreteRotation:
+    def __init__(self, angles):
+        self.angles = angles
+
+    def __call__(self, x):
+        angle = random.choice(self.angles)
+        return TF.rotate(x, angle)
 
 def download_bsd300(dest="./dataset"):
-    # return './dataset'
     output_image_dir = join(dest, "BSDS300/images")
+    # output_image_dir = join(dest, "../data")
 
-    if not exists(output_image_dir):
-        url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
-        print("downloading url ", url)
+    # if not exists(output_image_dir):
+    #     url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
+    #     print("downloading url ", url)
 
-        data = urllib.request.urlopen(url)
+    #     data = urllib.request.urlopen(url)
 
-        file_path = join(dest, basename(url))
-        with open(file_path, 'wb') as f:
-            f.write(data.read())
+    #     file_path = join(dest, basename(url))
+    #     with open(file_path, 'wb') as f:
+    #         f.write(data.read())
 
-        print("Extracting data")
-        with tarfile.open(file_path) as tar:
-            for item in tar:
-                tar.extract(item, dest)
+    #     print("Extracting data")
+    #     with tarfile.open(file_path) as tar:
+    #         for item in tar:
+    #             tar.extract(item, dest)
 
-        remove(file_path)
+    #     remove(file_path)
 
     return output_image_dir
 
@@ -39,6 +48,8 @@ def calculate_valid_crop_size(crop_size, upscale_factor):
 def input_transform(crop_size, upscale_factor):
     return Compose([
         CenterCrop(crop_size),
+        RandomDiscreteRotation([0, 90, 180, 270]),
+        RandomHorizontalFlip(),
         Resize(crop_size // upscale_factor, interpolation=3),
         ToTensor(),
     ])
@@ -47,6 +58,8 @@ def input_transform(crop_size, upscale_factor):
 def target_transform(crop_size):
     return Compose([
         CenterCrop(crop_size),
+        RandomDiscreteRotation([0, 90, 180, 270]),
+        RandomHorizontalFlip(),
         ToTensor(),
     ])
 
