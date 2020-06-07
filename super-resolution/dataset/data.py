1e623440b3615f57192eefadf15b2a4bdf43ad99
Alex Zhao <alexzhao21@gmail.com>
2020-06-07 16:03:39 -0700

    path

--- super-resolution/dataset/data.py
+++ super-resolution/dataset/data.py
@@ -19,7 +19,7 @@ class RandomDiscreteRotation:
 
 def download_bsd300(dest="./dataset"):
     # output_image_dir = join(dest, "BSDS300/images")
-    output_image_dir = join(dest, "../../data")
+    output_image_dir = join(dest, "..", "..", "data")
 
     # if not exists(output_image_dir):
     #     url = "http://www2.eecs.berkeley.edu/Research/Projects/CS/vision/bsds/BSDS300-images.tgz"
